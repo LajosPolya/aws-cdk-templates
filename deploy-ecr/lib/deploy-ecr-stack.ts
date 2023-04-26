e56ff68ebb736fb0794ae393a62d8e425e196418
LajosPolya <lajospolya94@gmail.com>
2023-04-25 23:28:56 -0400

    Add concept of env to the project

--- deploy-ecr/lib/deploy-ecr-stack.ts
+++ deploy-ecr/lib/deploy-ecr-stack.ts
@@ -1,12 +1,16 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 
+export interface DeployEcrStackProps extends cdk.StackProps {
+  envName: string;
+}
+
 export class DeployEcrStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: Construct, id: string, props: DeployEcrStackProps) {
     super(scope, id, props);
 
     const repository = new cdk.aws_ecr.Repository(this, 'Repository', {
-      repositoryName: 'micronaut-api',
+      repositoryName: `micronaut-api-${props?.envName}`,
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       imageScanOnPush: false,
       autoDeleteImages: true,
