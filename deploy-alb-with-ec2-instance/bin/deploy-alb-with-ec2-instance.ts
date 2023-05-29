c290582e54a040c050e8aacd4a503e99aacd959a
LajosPolya <lajospolya94@gmail.com>
2023-05-28 22:12:45 -0400

    Deploy ALB attached to two EC2 instances

---
+++ deploy-alb-with-ec2-instance/bin/deploy-alb-with-ec2-instance.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { DeployAlbWithEc2InstanceStack } from "../lib/deploy-alb-with-ec2-instance-stack";
+
+const app = new cdk.App();
+const scope = app.node.getContext("scope");
+const account = app.node.tryGetContext("account");
+const region = app.node.tryGetContext("region");
+new DeployAlbWithEc2InstanceStack(app, "DeployAlbWithEc2InstanceStack", {
+  stackName: `deploy-alb-with-ec2-instance-${scope}`,
+  scope,
+  env: {
+    account: account || process.env.CDK_DEFAULT_ACCOUNT,
+    region: region || process.env.CDK_DEFAULT_REGION,
+  },
+});
---
+++ deploy-alb-with-ec2-instance/test/deploy-alb-with-ec2-instance.test.ts
@@ -0,0 +1,16 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DeployAlbWithEc2Instance from '../lib/deploy-alb-with-ec2-instance-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/deploy-alb-with-ec2-instance-stack.ts
+test("SQS Queue Created", () => {
+  //   const app = new cdk.App();
+  //     // WHEN
+  //   const stack = new DeployAlbWithEc2Instance.DeployAlbWithEc2InstanceStack(app, 'MyTestStack');
+  //     // THEN
+  //   const template = Template.fromStack(stack);
+  //   template.hasResourceProperties('AWS::SQS::Queue', {
+  //     VisibilityTimeout: 300
+  //   });
+});
