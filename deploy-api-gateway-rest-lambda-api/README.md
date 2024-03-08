e89090c1299a0d75a30c1840cbf89526fb4a4370
LajosPolya <lajospolya94@gmail.com>
2024-03-07 21:56:40 -0500

    add second lambda for proxy paths

--- deploy-api-gateway-rest-lambda-api/README.md
+++ deploy-api-gateway-rest-lambda-api/README.md
@@ -24,11 +24,17 @@ This CDK app deploys an API Gateway REST API backed by Lambda.
 
 The app will set the environment (account and region) based on the environment variables `CDK_DEFAULT_ACCOUNT` and `CDK_DEFAULT_REGION` respectively. These environment variables are set using the default AWS CLI configurations, more information can be [here](https://docs.aws.amazon.com/cdk/v2/guide/environments.html). The app can be deployed to the non-default environment by updating the CDK context with values for `account` and `region`.
 
-This deploys an API Gateway REST API backed by Lambda. The API can be accessed by its URL which is exported by the CDK and therefore printed to the CLI when the app is deployed.
+This deploys an API Gateway REST API backed by Lambda. This API is backed by two instances of AWS Lambda and can be accessed by the API's URL which is exported by the CDK and therefore printed to the CLI when the app is deployed.
 
 ### cURL :curling_stone:
 
-`curl --location <api_url>`
+The first lambda can be accessed by the `parent` endpoint.
+
+`curl --location <api_url>/parent`
+
+The second lambda is a proxy lambda for `/parent/*` which means it will execute for any path that begins with `parent/`, for example:
+
+`curl --location <api_url>/parent/child`
 
 ### Browser :surfer:
 
--- deploy-api-gateway-rest-lambda-api/lib/deploy-api-gateway-rest-lambda-api-stack.ts
+++ deploy-api-gateway-rest-lambda-api/lib/deploy-api-gateway-rest-lambda-api-stack.ts
@@ -1,4 +1,5 @@
 import * as cdk from "aws-cdk-lib";
+import { RestApi } from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
 
 export interface DeployApiGatewayRestLambdaApiStackProps
@@ -15,7 +16,7 @@ export class DeployApiGatewayRestLambdaApiStack extends cdk.Stack {
     super(scope, id, props);
 
     // Lambda response must be in exactly this format
-    const inlineCode = cdk.aws_lambda.Code.fromInline(`
+    const parentInlineCode = cdk.aws_lambda.Code.fromInline(`
 exports.handler = async(event) => {
   console.log(JSON.stringify(event));
   return {
@@ -24,28 +25,73 @@ exports.handler = async(event) => {
     "headers": {
         "Content-Type": "application/json"
     },
-    "body": "Lambda Successfully executed. Check logs for additional info."
+    "body": "Parent Lambda Successfully executed. Check logs for additional info."
   };
 };    
     `);
 
-    const lambda = new cdk.aws_lambda.Function(this, "inlineCodeLambda", {
-      runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
-      code: inlineCode,
-      handler: "index.handler",
-      description:
-        "Lambda deployed with inline code and triggered by API Gateway",
-      timeout: cdk.Duration.seconds(3),
-      functionName: `lambdaTriggeredByApiGateway-${props.scope}`,
-      logRetention: cdk.aws_logs.RetentionDays.ONE_DAY,
-      retryAttempts: 0,
-    });
+    const parentLambda = new cdk.aws_lambda.Function(
+      this,
+      "inlineParentCodeLambda",
+      {
+        runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+        code: parentInlineCode,
+        handler: "index.handler",
+        description:
+          "Parent Lambda deployed with inline code and triggered by API Gateway",
+        timeout: cdk.Duration.seconds(3),
+        functionName: `lambdaTriggeredByApiGatewayParent-${props.scope}`,
+        logRetention: cdk.aws_logs.RetentionDays.ONE_DAY,
+        retryAttempts: 0,
+      },
+    );
+
+    // Lambda response must be in exactly this format
+    const proxyInlineCode = cdk.aws_lambda.Code.fromInline(`
+        exports.handler = async(event) => {
+          console.log(JSON.stringify(event));
+          return {
+            "isBase64Encoded": false,
+            "statusCode": 200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": "Proxy Lambda Successfully executed. Called by " + event.path + ". Check logs for additional info."
+          };
+        };    
+            `);
+
+    const proxyLambda = new cdk.aws_lambda.Function(
+      this,
+      "inlineProxyCodeLambda",
+      {
+        runtime: cdk.aws_lambda.Runtime.NODEJS_20_X,
+        code: proxyInlineCode,
+        handler: "index.handler",
+        description:
+          "Proxy Lambda deployed with inline code and triggered by API Gateway",
+        timeout: cdk.Duration.seconds(3),
+        functionName: `lambdaTriggeredByApiGatewayProxy-${props.scope}`,
+        logRetention: cdk.aws_logs.RetentionDays.ONE_DAY,
+        retryAttempts: 0,
+      },
+    );
 
     const api = new cdk.aws_apigateway.LambdaRestApi(this, "lambdaApi", {
-      handler: lambda,
+      handler: parentLambda,
       restApiName: `lambdaApi-${props.scope}`,
       endpointTypes: [cdk.aws_apigateway.EndpointType.REGIONAL],
       description: "Rest API backed by a lambda",
+      proxy: false,
+    });
+
+    const parentPath = api.root.addResource("parent");
+    parentPath.addMethod("GET");
+    const proxyLambdaIntegration = new cdk.aws_apigateway.LambdaIntegration(
+      proxyLambda,
+    );
+    const proxyPath = parentPath.addProxy({
+      defaultIntegration: proxyLambdaIntegration,
     });
   }
 }
