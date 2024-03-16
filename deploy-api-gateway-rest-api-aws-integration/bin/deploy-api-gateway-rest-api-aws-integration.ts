68bbf001ea8a052630ca8c583ac79ba74ec6df65
LajosPolya <lajospolya94@gmail.com>
2024-03-16 16:39:22 -0400

    API Gatway with AWS Integration (SQS)

---
+++ deploy-api-gateway-rest-api-aws-integration/bin/deploy-api-gateway-rest-api-aws-integration.ts
@@ -0,0 +1,21 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { DeployApiGatewayRestApiAwsIntegrationStack } from "../lib/deploy-api-gateway-rest-api-aws-integration-stack";
+
+const app = new cdk.App();
+const scope = app.node.getContext("scope");
+const account = app.node.tryGetContext("account");
+const region = app.node.tryGetContext("region");
+new DeployApiGatewayRestApiAwsIntegrationStack(
+  app,
+  "DeployApiGatewayRestApiAwsIntegrationStack",
+  {
+    stackName: `apiGatewayRestApiAwsInt-${scope}`,
+    scope,
+    env: {
+      account: account || process.env.CDK_DEFAULT_ACCOUNT,
+      region: region || process.env.CDK_DEFAULT_REGION,
+    },
+  },
+);
---
+++ deploy-api-gateway-rest-api-aws-integration/lib/deploy-api-gateway-rest-api-aws-integration-stack.ts
@@ -0,0 +1,73 @@
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export interface DeployApiGatewayRestApiAwsIntegrationStackStackProps
+  extends cdk.StackProps {
+  scope: string;
+}
+
+export class DeployApiGatewayRestApiAwsIntegrationStack extends cdk.Stack {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: DeployApiGatewayRestApiAwsIntegrationStackStackProps,
+  ) {
+    super(scope, id, props);
+
+    const api = new cdk.aws_apigateway.RestApi(this, "restApiAwsIntegration", {
+      endpointConfiguration: {
+        types: [cdk.aws_apigateway.EndpointType.REGIONAL],
+      },
+      restApiName: `restApiIntegration-${props.scope}`,
+    });
+
+    const principal = new cdk.aws_iam.ServicePrincipal(
+      "apigateway.amazonaws.com",
+    );
+    const role = new cdk.aws_iam.Role(this, "apiGatewayRole", {
+      assumedBy: principal,
+      roleName: `apiGatewayRole-${props.scope}`,
+      description: "Role assumed by API Gateway",
+    });
+
+    const queueName = `apiGatewayQueue-${props.scope}`;
+    const queue = new cdk.aws_sqs.Queue(this, "sqs", {
+      queueName: queueName,
+      retentionPeriod: cdk.Duration.hours(1),
+    });
+    queue.grantSendMessages(role);
+
+    /* Generates the request sent the SQS based on the request sent by the client.
+    Sets the query parameter `Action` to `SendMessage` on the request to SQS.
+    Sets the query parameter `MessageBody` to the request body from request sent by the client.
+    In the end the request has the following format:
+    https://<sqs_uri>/<account_id>/<queue_name>?Action=SendMessage&MessageBody=<cient_request_body> */
+    const sqsIntegration = new cdk.aws_apigateway.AwsIntegration({
+      service: "sqs",
+      path: queueName,
+      options: {
+        requestParameters: {
+          "integration.request.querystring.Action": "'SendMessage'",
+          "integration.request.querystring.MessageBody": "method.request.body",
+        },
+        integrationResponses: [
+          {
+            statusCode: "200",
+          },
+        ],
+        credentialsRole: role,
+        passthroughBehavior:
+          cdk.aws_apigateway.PassthroughBehavior.WHEN_NO_MATCH,
+      },
+    });
+
+    api.root.addMethod("POST", sqsIntegration, {
+      operationName: `forwardToSqs-${props.scope}`,
+      methodResponses: [
+        {
+          statusCode: "200",
+        },
+      ],
+    });
+  }
+}
