46c809ae2393163df2ccfc6e3747c21060d05a11
LajosPolya <lajospolya94@gmail.com>
2024-03-30 17:22:52 -0400

    cleanup deployer

--- deploy-vpc/bin/deploy-vpc.ts
+++ deploy-vpc/bin/deploy-vpc.ts
@@ -3,7 +3,6 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { DeployVpcStack } from "../lib/deploy-vpc-stack";
 import { DeployEc2Stack } from "../lib/deploy-ec2-stack";
-import { DeployPrivateLambdaStack } from "../lib/deploy-private-lambda-stack";
 
 const app = new cdk.App();
 const scope = app.node.getContext("scope");
@@ -12,18 +11,6 @@ const region = app.node.tryGetContext("region");
 const vpcStack = new DeployVpcStack(app, "DeployVpcStack", {
   stackName: `vpc-${scope}`,
   scope,
-  env: {
-    account: account || process.env.CDK_DEFAULT_ACCOUNT,
-    region: region || process.env.CDK_DEFAULT_REGION,
-  },
-});
-
-new DeployPrivateLambdaStack(app, "DeployPrivateLambdaStack", {
-  stackName: `vpcPrivateLambda-${scope}`,
-  scope,
-  vpcL1: vpcStack.vpcL1,
-  privateSubnet: vpcStack.privateWithEgressSubnet,
-  stackTags: vpcStack.stackTags,
   env: {
     account: account || process.env.CDK_DEFAULT_ACCOUNT,
     region: region || process.env.CDK_DEFAULT_REGION,
