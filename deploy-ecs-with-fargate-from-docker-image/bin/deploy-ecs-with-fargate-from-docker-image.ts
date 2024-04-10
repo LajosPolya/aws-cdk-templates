3be90c96fa89d98c725cd6566188fe8d902f07cd
LajosPolya <lajospolya94@gmail.com>
2024-04-10 00:00:59 -0400

    deploy ECS Fargate deployed from local docker image

---
+++ deploy-ecs-with-fargate-from-docker-image/bin/deploy-ecs-with-fargate-from-docker-image.ts
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import * as cdk from 'aws-cdk-lib';
+import { DeployEcsWithFargateFromDockerImageStack } from '../lib/deploy-ecs-with-fargate-from-docker-image-stack';
+
+const app = new cdk.App();
+const scope = app.node.getContext("scope");
+const account = app.node.tryGetContext("account");
+const region = app.node.tryGetContext("region");
+
+new DeployEcsWithFargateFromDockerImageStack(app, 'DeployEcsWithFargateFromDockerImageStack', {
+  stackName: `ecsWithFargate-${scope}`,
+  scope,
+  env: {
+    account: account || process.env.CDK_DEFAULT_ACCOUNT,
+    region: region || process.env.CDK_DEFAULT_REGION,
+  },
+});
---
+++ deploy-ecs-with-fargate-from-docker-image/lib/deploy-ecs-with-fargate-from-docker-image-stack.ts
@@ -0,0 +1,93 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+
+export interface DeployEcsWithFargateFromDockerImageStackProps extends cdk.StackProps {
+  scope: string;
+} 
+
+export class DeployEcsWithFargateFromDockerImageStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: DeployEcsWithFargateFromDockerImageStackProps) {
+    super(scope, id, props);
+
+    const vpc = new cdk.aws_ec2.Vpc(this, "vpc", {
+      ipAddresses: cdk.aws_ec2.IpAddresses.cidr(
+        cdk.aws_ec2.Vpc.DEFAULT_CIDR_RANGE
+      ),
+      availabilityZones: [`${props.env!.region!}a`],
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 16,
+          name: `ecsFargatePublic-${props.scope}`,
+          subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    const cluster = new cdk.aws_ecs.Cluster(this, "cluster", {
+      clusterName: `ecsFargateFromDockerImage-${props.scope}`,
+      vpc: vpc,
+      enableFargateCapacityProviders: true,
+    });
+
+    const fargateTaskDef = new cdk.aws_ecs.FargateTaskDefinition(
+      this,
+      "fargateTaskDefinition",
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+        family: `ecsFargateFromDockerImage-${props.scope}`,
+      }
+    );
+    fargateTaskDef.addContainer("apiContainer", {
+      image: cdk.aws_ecs.ContainerImage.fromAsset('../api'),
+      essential: true,
+      portMappings: [
+        {
+          containerPort: 8080,
+        },
+      ],
+      logging: cdk.aws_ecs.LogDrivers.awsLogs({
+        streamPrefix: `ecsFargate-${props.scope}`,
+        logGroup: new cdk.aws_logs.LogGroup(this, "logGroup", {
+          logGroupName: `/ecs-with-fargate-api-from-docker-image/${props.scope}`,
+          retention: cdk.aws_logs.RetentionDays.ONE_DAY,
+          removalPolicy: cdk.RemovalPolicy.DESTROY,
+        }),
+      }),
+    });
+
+    const securityGroup = new cdk.aws_ec2.SecurityGroup(
+      this,
+      "securityGroup",
+      {
+        securityGroupName: `ecsFargate-${props.scope}`,
+        description: "Allow all traffic",
+        vpc: vpc,
+      }
+    );
+    securityGroup.addIngressRule(
+      cdk.aws_ec2.Peer.anyIpv4(),
+      cdk.aws_ec2.Port.allTcp(),
+      "Allow all TCP"
+    );
+    new cdk.aws_ecs.FargateService(this, "fargateService", {
+      taskDefinition: fargateTaskDef,
+      assignPublicIp: true,
+      vpcSubnets: vpc.selectSubnets({
+        subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
+      }),
+      cluster: cluster,
+      desiredCount: 1,
+      serviceName: `fargateFromDockerImage-${props.scope}`,
+      platformVersion: cdk.aws_ecs.FargatePlatformVersion.VERSION1_4,
+      securityGroups: [securityGroup],
+    });
+
+    new cdk.CfnOutput(this, "clusterArn", {
+      description: "The ARN of the Fargate Cluster",
+      value: cluster.clusterArn,
+      exportName: `fargateClusterArn-${props.scope}`,
+    });
+  }
+}
