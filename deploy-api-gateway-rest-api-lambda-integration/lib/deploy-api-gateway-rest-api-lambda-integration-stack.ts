f4ef24be997218f59db26891d4f2d8a4c38f4997
LajosPolya <lajospolya94@gmail.com>
2024-04-23 23:10:21 -0400

    remove unused import

--- deploy-api-gateway-rest-api-lambda-integration/lib/deploy-api-gateway-rest-api-lambda-integration-stack.ts
+++ deploy-api-gateway-rest-api-lambda-integration/lib/deploy-api-gateway-rest-api-lambda-integration-stack.ts
@@ -1,5 +1,4 @@
 import * as cdk from "aws-cdk-lib";
-import { RestApi } from "aws-cdk-lib/aws-apigateway";
 import { Construct } from "constructs";
 
 export interface DeployApiGatewayRestApiLambdaIntegrationStackProps
