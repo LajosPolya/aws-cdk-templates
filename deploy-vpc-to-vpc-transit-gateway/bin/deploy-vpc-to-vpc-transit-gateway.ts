cc580ab2e4d725b30ec652894499d0368b307185
LajosPolya <lajospolya94@gmail.com>
2024-04-25 00:03:13 -0400

    rename vpv-to-vpc-network to vpc-to-vpc-transit-gateway

---
+++ deploy-vpc-to-vpc-transit-gateway/bin/deploy-vpc-to-vpc-transit-gateway.ts
@@ -0,0 +1,22 @@
+#!/usr/bin/env node
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { DeployVpcToVpcTransitGatewayStack } from "../lib/deploy-vpc-to-vpc-transit-gateway-stack";
+
+const app = new cdk.App();
+const scope = app.node.getContext("scope");
+const account = app.node.tryGetContext("account");
+const region = app.node.tryGetContext("region");
+
+new DeployVpcToVpcTransitGatewayStack(
+  app,
+  "DeployVpcToVpcTransitGatewayStack",
+  {
+    stackName: `vpcToVpcTransitGateway-${scope}`,
+    scope,
+    env: {
+      account: account || process.env.CDK_DEFAULT_ACCOUNT,
+      region: region || process.env.CDK_DEFAULT_REGION,
+    },
+  },
+);
