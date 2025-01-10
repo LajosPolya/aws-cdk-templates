57bd4a49689799ce8bd00c546e49cd2100a53911
LajosPolya <lajospolya94@gmail.com>
2025-01-09 19:14:18 -0500

    deploy standalone ecs fargate task

---
+++ deploy-ecs-with-fargate-standalone/lib/deploy-ecs-with-fargate-standalone-stack.ts
@@ -0,0 +1,104 @@
+import * as cdk from 'aws-cdk-lib';
+import { TaskDefinition } from 'aws-cdk-lib/aws-ecs';
+import { Construct } from 'constructs';
+
+export interface DeployEcsWithFargateStandaloneStackProps extends cdk.StackProps {
+  scope: string;
+}
+
+export class DeployEcsWithFargateStandaloneStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: DeployEcsWithFargateStandaloneStackProps) {
+    super(scope, id, props);
+
+    const vpc = new cdk.aws_ec2.Vpc(this, "vpc", {
+      ipAddresses: cdk.aws_ec2.IpAddresses.cidr(
+        cdk.aws_ec2.Vpc.DEFAULT_CIDR_RANGE,
+      ),
+      availabilityZones: [`${props.env!.region!}a`],
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 28,
+          name: `ecsFargatePrivate-${props.scope}`,
+          subnetType: cdk.aws_ec2.SubnetType.PRIVATE_WITH_EGRESS,
+        },
+        {
+          cidrMask: 28,
+          name: `ecsFargatePublic-${props.scope}`,
+          subnetType: cdk.aws_ec2.SubnetType.PUBLIC,
+        },
+      ],
+    });
+
+    const cluster = new cdk.aws_ecs.Cluster(this, "cluster", {
+      clusterName: `ecsFargateStandalone-${props.scope}`,
+      vpc: vpc,
+      enableFargateCapacityProviders: true,
+    });
+
+    const fargateTaskDef = new cdk.aws_ecs.FargateTaskDefinition(
+      this,
+      "fargateTaskDefinition",
+      {
+        cpu: 256,
+        memoryLimitMiB: 512,
+        family: `ecsFargateStandalone-${props.scope}`,
+      },
+    );
+    fargateTaskDef.addContainer("apiContainer", {
+      image: cdk.aws_ecs.ContainerImage.fromAsset("../batch-job-script"),
+      essential: true,
+      portMappings: [
+        {
+          containerPort: 8080,
+        },
+      ],
+      logging: cdk.aws_ecs.LogDrivers.awsLogs({
+        streamPrefix: `ecsFargate-${props.scope}`,
+        logGroup: new cdk.aws_logs.LogGroup(this, "logGroup", {
+          logGroupName: `/ecs-with-fargate-standalone/${props.scope}`,
+          retention: cdk.aws_logs.RetentionDays.ONE_DAY,
+          removalPolicy: cdk.RemovalPolicy.DESTROY,
+        }),
+      }),
+    });
+
+    const securityGroup = new cdk.aws_ec2.SecurityGroup(this, "securityGroup", {
+      securityGroupName: `ecsFargate-${props.scope}`,
+      description: "Allow all traffic",
+      vpc: vpc,
+    });
+    securityGroup.addIngressRule(
+      cdk.aws_ec2.Peer.anyIpv4(),
+      cdk.aws_ec2.Port.allTcp(),
+      "Allow all TCP",
+    );
+
+    new cdk.CfnOutput(this, "taskDefinitionFamily", {
+      description: "The Family Name of the Task Definition",
+      value: fargateTaskDef.family,
+      exportName: `taskDefinitionName-${props.scope}`,
+    });
+
+    new cdk.CfnOutput(this, "clusterName", {
+      description: "The name of the Fargate Cluster",
+      value: cluster.clusterName,
+      exportName: `fargateClusterName-${props.scope}`,
+    });
+
+    new cdk.CfnOutput(this, "securityGroupId", {
+      description: "The ID of the Security Group",
+      value: securityGroup.securityGroupId,
+      exportName: `securityGroupId-${props.scope}`,
+    });
+
+    new cdk.CfnOutput(this, "taskSubnetId", {
+      description: "The ID of the Subnet",
+      value: vpc.privateSubnets[0].subnetId,
+      exportName: `subnetId-${props.scope}`,
+    });
+
+    // Output task definition, public subnet id, sg id, etc
+    // aws ecs run-task --task-definition ecsFargateStandalone-lajos --cluster ecsFargateStandalone-lajos --network-configuration "awsvpcConfiguration={subnets=['subnet-0c43e5a96c0b23487'],securityGroups=['sg-0ef6ca47b54664365'],assignPublicIp=ENABLED}" --launch-type FARGATE
+  }
+}
